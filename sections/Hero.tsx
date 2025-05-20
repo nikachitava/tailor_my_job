bee5c9aee5edb9f12a957c6f1ba77ff387e0394b
nikachitava <chitava18.official@gmail.com>
2025-05-20 20:04:37 +0400

    create sections folder

---
+++ sections/Hero.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import CustomButton from "@/components/CustomButton";
+import React from "react";
+import heroAnimation from "../public/assets/heroAnimation.json";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), {
+    ssr: false,
+});
+
+const Hero = () => {
+    return (
+        <section className="bg-background text-foreground py-20 px-4 md:px-10">
+            <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-10">
+                <div className="flex-1 text-center md:text-left">
+                    <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+                        Tailor Your Job Applications
+                        <br className="hidden md:block" /> with{" "}
+                        <span className="text-primary">AI</span>
+                    </h1>
+                    <p className="mt-6 text-lg text-muted max-w-lg">
+                        Generate personalized CVs and cover letters that match
+                        the job description in seconds. Boost your chances of
+                        landing interviews with smarter applications.
+                    </p>
+                    <div className="mt-8 flex justify-center md:justify-start gap-4">
+                        <CustomButton
+                            title="Get Started"
+                            onClick={() => {
+                                console.log("clicked");
+                            }}
+                            className="bg-primary hover:bg-accent text-white px-6 py-3 rounded-xl font-medium transition"
+                        />
+                        <CustomButton
+                            title="See How It Works"
+                            onClick={() => {
+                                console.log("clicked");
+                            }}
+                            className="order border-input-border text-foreground px-6 py-3 rounded-xl font-medium hover:bg-[var(--accent)] hover:text-white transition"
+                        />
+                    </div>
+                </div>
+                {/* Animated SVG on right side */}
+                <div className="flex-1">
+                    <Lottie animationData={heroAnimation} loop />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
